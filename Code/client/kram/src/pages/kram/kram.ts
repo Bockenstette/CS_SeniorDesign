96aa120f46c5572d73fd862380995b4f5b4c30d0
thoma3mw <matthew.thomas0411@gmail.com>
2018-03-16 23:17:55 -0400

    Updates, popover, etc

--- Code/client/kram/src/pages/kram/kram.ts
+++ Code/client/kram/src/pages/kram/kram.ts
@@ -1,6 +1,53 @@
 import { Component } from '@angular/core';
-import { App, MenuController, NavController, PopoverController } from 'ionic-angular';
+import { App, MenuController, NavController, PopoverController, ViewController, AlertController } from 'ionic-angular';
 import { ItemPage } from '../item/item';
+
+@Component({
+  template: `
+    <ion-list>
+      <button ion-item (click)="close()">Refresh</button>
+      <button ion-item (click)="showRadio()">Sort by...</button>
+    </ion-list>
+  `
+})
+export class PopoverPage {
+  constructor(public viewCtrl: ViewController, public alertCtrl: AlertController) {}
+
+  close() {
+    this.viewCtrl.dismiss();
+  }
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Sort By');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Alphabetical',
+      value: 'alphabetical',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Location',
+      value: 'location'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Price',
+      value: 'price'
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK'
+    });
+
+    this.close();
+    alert.present();
+  }
+}
 
 @Component({
   selector: 'page-kram',
@@ -27,5 +74,11 @@ export class KramPage {
     } else {
       this.showSearchbar = false;
     }
+  }
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
   }
 }
