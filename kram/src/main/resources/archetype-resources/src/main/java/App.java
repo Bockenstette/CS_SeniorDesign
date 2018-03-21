d5c45d7dd395f2929ab91cd59ed932ea8f461909
Bockenstette <bockenstette_kevin@live.com>
2018-03-21 16:16:15 -0400

    Project Barebones/Reorganization of repo

---
+++ kram/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.kram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
