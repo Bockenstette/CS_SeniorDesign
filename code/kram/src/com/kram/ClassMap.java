222133e3fb52bfcf1b239da42ad4bf2d7750b3ff
Bockenstette <bockenstette_kevin@live.com>
2018-03-27 20:21:37 -0400

    GET's work on test server, pushing for milestone

---
+++ code/kram/src/com/kram/ClassMap.java
@@ -0,0 +1,67 @@
+package com.kram;
+
+import java.util.HashMap;
+
+public class ClassMap extends HashMap< String, Object >
+{
+	private static final long serialVersionUID = 1L;
+
+	private class Value
+	{
+		private Class< ? >	type;
+		private String		sqlType;
+
+		public Class< ? > getType()
+		{
+			return type;
+		}
+
+		public String getSqlType()
+		{
+			return sqlType;
+		}
+
+		// TODO: Pretty unsafe way of doing this, but good enough for now
+		public Value( Class< ? > type )
+		{
+			this.type = type;
+			switch ( type.getName() )
+			{
+			case "java.lang.String":
+				sqlType = "VARCHAR( 255 )";
+				break;
+			case "int":
+				sqlType = "INTEGER";
+				break;
+			case "float":
+				sqlType = "DOUBLE";
+				break;
+			case "double":
+				sqlType = "DECIMAL( 8, 2 )";
+				break;
+			}
+		}
+	};
+
+	private HashMap< String, Value > classMap = null;
+
+	public ClassMap()
+	{
+		classMap = new HashMap< String, Value >();
+	}
+
+	public void put( String key, Class< ? > value )
+	{
+		classMap.put( key, new Value( value ) );
+	}
+
+	public Class< ? > getType( String key )
+	{
+		return classMap.get( key ).getType();
+	}
+
+	public String getSqlType( String key )
+	{
+		return classMap.get( key ).getSqlType();
+	}
+}
--- code/kram/src/com/kram/Kram.java
+++ code/kram/src/com/kram/Kram.java
@@ -1,15 +1,32 @@
 package com.kram;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/db")
+import com.kram.database.DatabaseManager;;
+
+@Path( "/db" )
 public class Kram
 {
 	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String returnTitle()
+	@Produces( MediaType.TEXT_HTML )
+	public String printText()
 	{
-		return "<p>Java Web Service</p>";
+		return "<p> Within Java Code </p>";
+	}
+
+	@Path( "/settable/{username}" )
+	@GET
+	@Produces( MediaType.TEXT_HTML )
+	public String createTable( @PathParam( "username" ) String username )
+	{
+		DatabaseManager dbManager = DatabaseManager.getInstance();
+
+		dbManager.setTable( username );
+
+		return dbManager.showTablesHTML();
 	}
 }
